d967d1b79dc159aaae8224edee9b1df3749fb795
Leo Fzp <Fernandezpatrijuanl93@gmail.com>
2026-05-26 05:02:44 -0300

    add editar & excluir

--- GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
+++ GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
@@ -41,4 +41,50 @@ public class FabricanteController : Controller
 
         return RedirectToAction(nameof(Listar));
     }
+
+    [HttpGet]
+    public ActionResult Editar(string id)
+    {
+        Fabricante? fabricante = repositorioFabricante.SelecionarPorId(id);
+
+        if(fabricante == null)
+            return RedirectToAction(nameof(Listar));
+        
+        return View(fabricante);
+    }
+
+    [HttpPost]
+    public ActionResult Editar(string id, string nome, string email, string telefone)
+    {   
+        Fabricante fabricanteAtualizado = new Fabricante(nome, email, telefone);
+
+        repositorioFabricante.Editar(id, fabricanteAtualizado);
+        
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult Excluir(string id)
+    {
+        Fabricante? fabricante = repositorioFabricante.SelecionarPorId(id);
+
+        if (fabricante == null)
+            return RedirectToAction(nameof(Listar));
+
+        return View(fabricante);
+    }
+
+    [HttpPost]
+    [ActionName("Excluir")]
+    public ActionResult ExcluirConfirmado(string id)
+    {
+        Fabricante? fabricante = repositorioFabricante.SelecionarPorId(id);
+
+        if (fabricante == null)
+            return RedirectToAction(nameof(Listar));
+
+        repositorioFabricante.Excluir(fabricante);
+
+        return RedirectToAction(nameof(Listar));
+    }
 }
